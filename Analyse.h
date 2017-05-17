e6842dd5a7439952e2ce5b8f2487b3decc4c6df9
Aurore <aurore.loiseau@insa-lyon.fr>
2017-05-17 16:48:49 +0200

    Constructeurs par defaut remis

---
+++ Analyse.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include <map>
+#include "Genome.h"
+using namespace std;
+class Analyse
+{
+public:
+	Analyse(Genome genome, string type, string version);
+	Analyse();
+	~Analyse();
+
+public:
+	void addResult(string maladie, bool resultat);
+	void afficher();
+	Genome getGenome();
+
+public:
+	string type;
+	string version;
+	map<string, bool> resultats;
+	Genome genome;
+};
+
+
